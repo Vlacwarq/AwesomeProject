c8b426658ab70eb03359d0e9ddf61152d2191158
Vlacwarq <vlad12345676123@gmail.com>
2022-02-18 20:53:32 +0900

    test commit 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test Txt</Text>
+      <Text>Test Tdwdawawawawawawawawawadaxt</Text>
       <StatusBar style="auto" />
     </View>
   );
