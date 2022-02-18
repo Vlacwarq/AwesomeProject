fad562487c4939e1df8c2c9fd6060a26e51f2d61
Vlacwarq <vlad12345676123@gmail.com>
2022-02-18 21:17:56 +0900

    d

--- App.js
+++ App.js
@@ -7,12 +7,8 @@ export default function App() {
       <Text>Test Txt</Text>
       <Text>Masha</Text>
       <Text>Masha</Text>
-<<<<<<< HEAD
       <Text>Vlad 2</Text>
       <Text>Vlad 12</Text>
-=======
-      <Text>Masha</Text>
->>>>>>> d20a6010306221f6c5611b7bc9d03b287f5489fb
       <StatusBar style="auto" />
     </View>
   );
