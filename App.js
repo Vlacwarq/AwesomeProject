0d5af24d9fa523f17a6a6810b0a68d505b595630
MashaN2004 <67993634+MashaN2004@users.noreply.github.com>
2022-02-18 21:01:36 +0900

    masha

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Test Txt</Text>
+      <Text>Masha</Text>
       <StatusBar style="auto" />
     </View>
   );
